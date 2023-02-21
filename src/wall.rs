87f952b2290a14354c04f045f615918735e819cd
Linwei Zhang <lewiszlw520@gmail.com>
2023-02-21 19:24:57 +0800

    Refactor spawning wall

--- src/wall.rs
+++ src/wall.rs
@@ -1,5 +1,5 @@
-use bevy::prelude::*;
-use bevy_rapier2d::prelude::Collider;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy_rapier2d::prelude::{Collider, RigidBody};
 
 pub const WALL_THICKNESS: f32 = 10.0;
 // 墙壁x轴和y轴坐标
@@ -8,72 +8,81 @@ pub const RIGHT_WALL: f32 = 450.;
 pub const BOTTOM_WALL: f32 = -300.;
 pub const TOP_WALL: f32 = 300.;
 
-// 墙壁bundle
-#[derive(Bundle)]
-pub struct WallBundle {
-    #[bundle]
-    sprite_bundle: SpriteBundle, // 嵌套bundle
-}
+pub fn setup_wall(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let arena_height = TOP_WALL - BOTTOM_WALL;
+    let arena_width = RIGHT_WALL - LEFT_WALL;
+    let wall_color = Color::rgb(0.8, 0.8, 0.8);
+    let material_handle = materials.add(wall_color.into());
 
-// 墙壁位置
-pub enum WallLocation {
-    Left,
-    Right,
-    Bottom,
-    Top,
-}
+    // left wall
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(
+                    shape::Quad::new(Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS))
+                        .into(),
+                )
+                .into(),
+            material: material_handle.clone(),
+            transform: Transform::from_translation(Vec3::new(LEFT_WALL, 0., 0.)),
+            ..default()
+        },
+        RigidBody::Fixed,
+        Collider::cuboid(WALL_THICKNESS / 2.0, (arena_height + WALL_THICKNESS) / 2.0),
+    ));
 
-impl WallLocation {
-    fn position(&self) -> Vec2 {
-        match self {
-            WallLocation::Left => Vec2::new(LEFT_WALL, 0.),
-            WallLocation::Right => Vec2::new(RIGHT_WALL, 0.),
-            WallLocation::Bottom => Vec2::new(0., BOTTOM_WALL),
-            WallLocation::Top => Vec2::new(0., TOP_WALL),
-        }
-    }
+    // right wall
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(
+                    shape::Quad::new(Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS))
+                        .into(),
+                )
+                .into(),
+            material: material_handle.clone(),
+            transform: Transform::from_translation(Vec3::new(RIGHT_WALL, 0., 0.)),
+            ..default()
+        },
+        RigidBody::Fixed,
+        Collider::cuboid(WALL_THICKNESS / 2.0, (arena_height + WALL_THICKNESS) / 2.0),
+    ));
 
-    fn size(&self) -> Vec2 {
-        let arena_height = TOP_WALL - BOTTOM_WALL;
-        let arena_width = RIGHT_WALL - LEFT_WALL;
-        // Make sure we haven't messed up our constants
-        assert!(arena_height > 0.0);
-        assert!(arena_width > 0.0);
+    // top wall
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(
+                    shape::Quad::new(Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS))
+                        .into(),
+                )
+                .into(),
+            material: material_handle.clone(),
+            transform: Transform::from_translation(Vec3::new(0.0, TOP_WALL, 0.)),
+            ..default()
+        },
+        RigidBody::Fixed,
+        Collider::cuboid((arena_width + WALL_THICKNESS) / 2.0, WALL_THICKNESS / 2.0),
+    ));
 
-        match self {
-            WallLocation::Left | WallLocation::Right => {
-                Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS)
-            }
-            WallLocation::Bottom | WallLocation::Top => {
-                Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS)
-            }
-        }
-    }
-}
-
-impl WallBundle {
-    pub fn new(location: WallLocation) -> WallBundle {
-        WallBundle {
-            sprite_bundle: SpriteBundle {
-                transform: Transform {
-                    translation: location.position().extend(0.0), // z轴坐标0
-                    scale: location.size().extend(1.0), // https://github.com/bevyengine/bevy/issues/4149
-                    ..default()
-                },
-                sprite: Sprite {
-                    color: Color::rgb(0.8, 0.8, 0.8),
-                    ..default()
-                },
-                ..default()
-            },
-            // collider: Collider,
-        }
-    }
-}
-
-pub fn setup_wall(mut commands: Commands) {
-    commands.spawn(WallBundle::new(WallLocation::Left));
-    commands.spawn(WallBundle::new(WallLocation::Right));
-    commands.spawn(WallBundle::new(WallLocation::Bottom));
-    commands.spawn(WallBundle::new(WallLocation::Top));
+    // bottom wall
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(
+                    shape::Quad::new(Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS))
+                        .into(),
+                )
+                .into(),
+            material: material_handle.clone(),
+            transform: Transform::from_translation(Vec3::new(0.0, BOTTOM_WALL, 0.)),
+            ..default()
+        },
+        RigidBody::Fixed,
+        Collider::cuboid((arena_width + WALL_THICKNESS) / 2.0, WALL_THICKNESS / 2.0),
+    ));
 }
